0a4235c67f2eed414c6f5f9e6c54ec1c32e8e9cb
MLIDDOMAIN1\prietja <Jan_Henry_Prieto@manulife.ca>
2021-12-21 17:52:43 +0800

    Revising my flyway training project and fixed the errors.

--- sql/V003__Insert employee sample data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO employees (first_name, last_name) VALUES
-('Ronan', 'Scott'),
-('Hugo', 'Chen'),
-('Diego', 'Kabayo');
---
+++ sql/V005__Insert employees sample data.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees (first_name, last_name, title_id) VALUES
+('Nardong', 'Putik', 1),
+('Pepeng', 'Agimat', 2),
+('Asiong', 'Salonga', 3);
